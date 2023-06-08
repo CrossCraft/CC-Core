9e5a447d5d5bb823ffcc23cfc162c4020107cf3e
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2023-06-08 18:07:30 -0400

    Add trees

--- src/CC/world.c
+++ src/CC/world.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <stdio.h>
 
 #define FNL_IMPL
 #include <FNL.h>
@@ -23,7 +24,7 @@ void CC_World_Init(size_t x, size_t y, size_t z) {
     FNL_GLOBAL_state = fnlCreateState();
     FNL_GLOBAL_state.seed = CC_GLOBAL_world_seed;
     FNL_GLOBAL_state.noise_type = FNL_NOISE_PERLIN;
-    FNL_GLOBAL_state.frequency = 0.05f;
+    FNL_GLOBAL_state.frequency = 0.04f;
 }
 
 void CC_World_Term(void) {
@@ -31,6 +32,81 @@ void CC_World_Term(void) {
 }
 
 const float WORLD_HEIGHT = 64.0f;
+
+
+void setBlock(int x, int y, int z, block_t blockType) {
+    CC_World_SetBlock(x, y, z, blockType);
+}
+
+void growTree(int x, int y, int z, int height) {
+    int maxY = y + height;
+    int maxLeafY = maxY + 1;
+    int minLeaf = maxLeafY - 4;
+
+    // Set the leaves
+    for (int dx = -2; dx <= 2; dx++) {
+        for (int dz = -2; dz <= 2; dz++) {
+            for(int dy = 0; dy < height; dy++) {
+                if(dy < 2) {
+                    setBlock(x + dx, minLeaf + dy, z + dz, BLK_Leaves);
+
+                    if((dx == 2 && dz == 2) || (dx == -2 && dz == 2) || (dx == 2 && dz == -2) || (dx == -2 && dz == -2)) {
+                        if(rand() % 2) {
+                            setBlock(x + dx, minLeaf + dy, z + dz, BLK_Air);
+                        }
+                    }
+                }
+
+                if(dy == 2) {
+                    if((abs(dx) == 1 || abs(dz) == 1) && (abs(dx) + abs(dz)) ==  1) {
+                        setBlock(x + dx, minLeaf + dy, z + dz, BLK_Leaves);
+                    }
+
+                    if((dx == 1 && dz == 1) || (dx == -1 && dz == 1) || (dx == 1 && dz == -1) || (dx == -1 && dz == -1)) {
+                        if(rand() % 2) {
+                            setBlock(x + dx, minLeaf + dy, z + dz, BLK_Leaves);
+                        }
+                    }
+                }
+
+                if(dy == 3) {
+                    if((abs(dx) == 1 || abs(dz) == 1) && (abs(dx) + abs(dz)) ==  1) {
+                        setBlock(x + dx, minLeaf + dy, z + dz, BLK_Leaves);
+                    }
+                }
+            }
+        }
+    }
+
+    // Set the trunk
+    for (int i = 0; i < height; i++) {
+        setBlock(x, y + i, z, BLK_Logs);
+    }
+}
+
+void CC_World_Make_Tree(size_t ix, size_t iy, size_t iz) {
+    int tree_height = 4 + rand() % 3;
+
+    int lowx = ix - 2;
+    int lowz = iz - 2;
+    int highx = ix + 2;
+    int highz = iz + 2;
+
+    // Check if there is enough space for the tree
+    for(int x = lowx; x <= highx; x++) {
+        for(int z = lowz; z <= highz; z++) {
+            for(int y = iy; y < (int)(iy + tree_height); y++) {
+                block_t block;
+                CC_World_GetBlock(x, y, z, &block);
+                if(block != BLK_Air) {
+                    return;
+                }
+            }
+        }
+    }
+
+    growTree(ix, iy, iz, tree_height);
+}
 
 void CC_World_Generate(void) {
     size_t x, y, z;
@@ -88,6 +164,24 @@ void CC_World_Generate(void) {
                 }
             }
 
+        }
+    }
+
+    // Post-generation
+    srand(CC_GLOBAL_world_seed);
+    for(x = 0; x < CC_GLOBAL_world_data.x; x++) {
+        for(z = 0; z < CC_GLOBAL_world_data.z; z++) {
+            h = (size_t)noiseMap[z * CC_GLOBAL_world_data.x + x];
+            idx = CC_WIDX(x, h - 1, z, &CC_GLOBAL_world_data);
+
+            block_t check = CC_GLOBAL_world_data.blocks[idx];
+            idx = CC_WIDX(x, h, z, &CC_GLOBAL_world_data);
+
+            if(check == BLK_Grass) {
+                if((rand() % 100) < 10) {
+                    CC_World_Make_Tree(x, h, z);
+                }
+            }
         }
     }
 
