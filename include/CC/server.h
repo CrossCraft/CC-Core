4ac2f040a0434eafd91345e9d61a6c38a2a9132d
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2023-06-24 20:55:41 -0400

    Server Break Place

---
+++ include/CC/server.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <CC/eventloop.h>
+
+void CC_Server_Register_Handlers(EventLoop* event_loop);
--- src/CC/core.c
+++ src/CC/core.c
@@ -1,9 +1,8 @@
 #include <CC/core.h>
 #include <CC/entity.h>
 #include <CC/eventloop.h>
-#include <stdio.h>
 #include <string.h>
-#include <CC/eventpackets.h>
+#include <CC/server.h>
 
 static EventLoop * CC_GLOBAL_event_loop = NULL;
 
@@ -23,70 +22,11 @@ void CC_Core_Init(void) {
     CC_Player_Init();
 }
 
-void CC_Server_Handle_PlayerPositionAndLook(void* loop, EventPacket* packet) {
-    (void)loop; // DISCARD LOOP
-
-    PlayerData* data = CC_Player_GetData();
-    data->x = packet->data.player_position_and_look_cs.x;
-    data->y = packet->data.player_position_and_look_cs.y;
-    data->z = packet->data.player_position_and_look_cs.z;
-
-    data->pitch = packet->data.player_position_and_look_cs.pitch;
-    data->yaw = packet->data.player_position_and_look_cs.yaw;
-
-    data->on_ground = packet->data.player_position_and_look_cs.on_ground;
-}
-
-void CC_Server_Handle_PlayerRespawn(void* loop, EventPacket* packet) {
-    (void)packet; // DISCARD LOOP
-
-    CC_Player_Respawn();
-    EventPacket spawnPosition = CC_EventPacket_Create_SpawnPosition(128, 40, 128);
-    CC_EventLoop_PushPacketOutbound(loop, &spawnPosition);
-
-    EventPacket playerPositionAndLook = CC_EventPacket_Create_PlayerPositionAndLookServer(128, 40, 128, 0, 0, false);
-    CC_EventLoop_PushPacketOutbound(loop, &playerPositionAndLook);
-
-    EventPacket updateHealth = CC_EventPacket_Create_UpdateHealthServer();
-    CC_EventLoop_PushPacketOutbound(loop, &updateHealth);
-}
-
-void CC_Server_Handle_LoginClient(void* loop, EventPacket* packet) {
-    printf("Client Logging In Username: %s\n", packet->data.login_cs.username.data);
-    printf("Client Logging In Password: %s\n", packet->data.login_cs.password.data);
-
-    EventPacket packet_response = CC_EventPacket_Create_LoginServer();
-
-    EventLoop* eloop = (EventLoop*)loop;
-    CC_EventLoop_PushPacketOutbound(eloop, &packet_response);
-
-    EventPacket spawnPosition = CC_EventPacket_Create_SpawnPosition(128, 40, 128);
-    CC_EventLoop_PushPacketOutbound(eloop, &spawnPosition);
-
-    EventPacket playerPositionAndLook = CC_EventPacket_Create_PlayerPositionAndLookServer(128, 40, 128, 0, 0, false);
-    CC_EventLoop_PushPacketOutbound(eloop, &playerPositionAndLook);
-
-    EventPacket updateHealth = CC_EventPacket_Create_UpdateHealthServer();
-    CC_EventLoop_PushPacketOutbound(eloop, &updateHealth);
-}
-
-void CC_Server_Handle_Handshake(void* loop, EventPacket* packet) {
-    printf("Client Connecting Username: %s\n", packet->data.handshake_cs.username.data);
-
-    EventPacket packet_response = CC_EventPacket_Create_Handshake("", true);
-
-    EventLoop* eloop = (EventLoop*)loop;
-    CC_EventLoop_PushPacketOutbound(eloop, &packet_response);
-}
-
 void CC_Core_SetEventLoop(EventLoop * event_loop) {
     CC_GLOBAL_event_loop = event_loop;
     CC_EventLoop_SetServer(event_loop, true);
 
-    CC_EventLoop_RegisterHandler(event_loop, CC_PACKET_TYPE_HANDSHAKE, CC_Server_Handle_Handshake);
-    CC_EventLoop_RegisterHandler(event_loop, CC_PACKET_TYPE_LOGIN, CC_Server_Handle_LoginClient);
-    CC_EventLoop_RegisterHandler(event_loop, CC_PACKET_TYPE_PLAYER_POSITION_AND_LOOK, CC_Server_Handle_PlayerPositionAndLook);
-    CC_EventLoop_RegisterHandler(event_loop, CC_PACKET_TYPE_RESPAWN, CC_Server_Handle_PlayerRespawn);
+    CC_Server_Register_Handlers(CC_GLOBAL_event_loop);
 }
 
 static float CC_GLOBAL_delta_time = 0.0f;
